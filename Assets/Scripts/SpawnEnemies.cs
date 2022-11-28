94b016f6bd2dfc9dd8b62f09625633242ced6395
Михаил Цаплин <mitsagames@gmail.com>
2022-11-28 10:50:04 +0500

    Add enemy asset

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnPointsRoot;
+
+}
